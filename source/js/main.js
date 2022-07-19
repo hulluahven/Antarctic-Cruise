c8e9c9f9820dfc8ad6cf47677820cb4934832435
hulluahven <hulluahven@gmail.com>
2022-07-19 17:28:51 +0300

    шапка и скрипт

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,10 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 
+const pageHeader = document.querySelector('[data-header]');
+const menuButton = document.querySelector('[data-button]');
+const mainNav = document.querySelector('[data-nav]');
+const promoBlock = document.querySelector('[data-promo]');
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -9,9 +13,54 @@ window.addEventListener('DOMContentLoaded', () => {
   // ---------------------------------
 
   iosVhFix();
+  // console.log(pageHeader.closest('[data-header]'))
 
   // Modules
   // ---------------------------------
+
+  if (pageHeader.closest('[data-header]') && pageHeader.classList.contains('is-closed')) {
+    pageHeader.classList.remove('is-closed');
+  } else {
+    pageHeader.classList.add('is-closed');
+  }
+
+
+  if (mainNav.closest('[data-nav]') && mainNav.classList.contains('is-open')) {
+    mainNav.classList.remove('is-open');
+    mainNav.classList.add('is-closed');
+  }
+
+  if (promoBlock.closest('[data-promo]') && promoBlock.classList.contains('is-hidden')) {
+    promoBlock.classList.remove('is-hidden');
+  } else {
+    promoBlock.classList.add('is-hidden');
+  }
+
+  if (mainNav.closest('[data-nav]')) {
+    menuButton.addEventListener('click', function () {
+      if (mainNav.classList.contains('is-closed')) {
+        mainNav.classList.remove('is-closed');
+        mainNav.classList.add('is-open');
+      } else {
+        mainNav.classList.remove('is-open');
+        mainNav.classList.add('is-closed');
+      }
+
+      if (pageHeader.classList.contains('is-closed')) {
+        pageHeader.classList.remove('is-closed');
+        pageHeader.classList.add('is-open');
+      } else {
+        pageHeader.classList.add('is-closed');
+        pageHeader.classList.remove('is-open');
+      }
+
+      if (promoBlock.classList.contains('is-hidden')) {
+        promoBlock.classList.remove('is-hidden');
+      } else {
+        promoBlock.classList.add('is-hidden');
+      }
+    });
+  }
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
